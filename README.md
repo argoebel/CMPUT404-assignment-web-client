8d2b68950281fb1e2ec472ec8230a1edafed66b7
Austin Goebel <argoebel@ualberta.ca>
2020-02-05 11:27:11 -0700

    all tests working

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-CMPUT404-assignment-web-client
-==============================
+# CMPUT404-assignment-web-client
 
 CMPUT404-assignment-web-client
 
@@ -7,10 +6,9 @@ See requirements.org (plain-text) for a description of the project.
 
 Make a simple web-client like curl or wget
 
-Contributors / Licensing
-========================
+# Contributors / Licensing
 
-Generally everything is LICENSE'D under the Apache 2 license by Abram Hindle, 
+Generally everything is LICENSE'D under the Apache 2 license by Abram Hindle,
 https://github.com/tywtyw2002, and https://github.com/treedust
 
 But the server.py example is derived from the python documentation
@@ -18,3 +16,8 @@ examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+Sources:
+
+- https://stackoverflow.com/questions/5607551/how-to-urlencode-a-querystring-in-python, bgporter
+- https://docs.python.org/3/library/urllib.parse.html
+- https://docs.python.org/3/library/json.html
--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,16 +24,23 @@ import re
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
+
 
 class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
 
+
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    def __init__(self):
+        self.code = 500
+        self.body = ''
+
+    # def get_host_port(self,url):
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,55 +48,134 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        self.code = int(data.split(' ')[1])
 
-    def get_headers(self,data):
-        return None
+    def get_headers(self, data):
+        # split data into its lines
+        self.split_data = data.split('\r\n')
+        i = 1
+        self.headers = []
+        # iterate through all potential headers in request
+        while self.split_data[i] != '':
+            self.headers.append(self.split_data[i])
+            i += 1
+        # print(self.headers)
 
     def get_body(self, data):
-        return None
-    
+        # ensure that data variable is valid
+        try:
+            self.body = data.split("\r\n\r\n")[1]
+        except:
+            self.body = ''
+
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
-        
+
     def close(self):
         self.socket.close()
 
+    def parse_input_url(self, url):
+        parsed_url = urllib.parse.urlparse(url)
+        self.scheme = parsed_url.scheme
+        self.host = parsed_url.hostname
+        self.path = parsed_url.path
+        self.params = parsed_url.params
+        self.query = parsed_url.query
+        self.fragment = parsed_url.fragment
+        self.port = parsed_url.port
+
+        if self.port == None:
+            if self.scheme == "https":
+                self.port = 443
+            else:
+                self.port = 80
+
+        if self.path == '':
+            self.path = '/'
+
+        # print(self.scheme)
+        # print(self.host)
+        # print(self.port)
+        # print(self.path)
+        # print(self.params)
+        # print(self.query)
+        # print(self.fragment)
+
     # read everything from the socket
+
     def recvall(self, sock):
         buffer = bytearray()
         done = False
         while not done:
-            part = sock.recv(1024)
+            part = sock.recv(4096)
             if (part):
                 buffer.extend(part)
             else:
                 done = not part
+        # print(buffer)
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        # GET request
+        self.parse_input_url(url)
+        self.connect(self.host, self.port)
+        self.request = "GET " + self.path + " HTTP/1.1\r\nHost: " + \
+            self.host + "\r\nConnection: close\r\n\r\n"
+        self.sendall(self.request)
+        self.data = self.recvall(self.socket)
+        self.get_code(self.data)
+        self.get_headers(self.data)
+        self.get_body(self.data)
+        # print("GET SELF.BODY: [%s]" % self.body)
+        self.close()
+
+        print(self.body)
+        return HTTPResponse(self.code, self.body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+        # print("POST REQUEST")
+        self.parse_input_url(url)
+        self.connect(self.host, self.port)
+
+        if args == None:
+            self.request = "POST " + self.path + " HTTP/1.1\r\nHost: " + \
+                self.host + "\r\nContent-length: 0\r\nConnection: close\r\n\r\n"
+        else:
+            # https://docs.python.org/3/library/urllib.parse.html
+            # https://stackoverflow.com/questions/40557606/how-to-url-encode-in-python-3
+            self.request = "POST " + self.path + " HTTP/1.1\r\nHost: " + \
+                self.host + "\r\nConnection: close\r\nContent-length: " + str(len(urllib.parse.urlencode(
+                    args))) + "\r\nContent-Type: application/json\r\n\r\n" + urllib.parse.urlencode(args)
+        # print("SELF.REQUEST: [%s]" % self.request)
+        self.sendall(self.request)
+        self.data = self.recvall(self.socket)
+        # print("SELF.DATA: [%s]" % self.data)
+        self.close()
+        self.get_code(self.data)
+        self.get_headers(self.data)
+        self.get_body(self.data)
+        # print("SELF.BODY")
+        print(self.body)
+        # https://docs.python.org/3/library/json.html
+        return HTTPResponse(self.code, self.body)
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
-            return self.POST( url, args )
+            # print("POST")
+            return self.POST(url, args)
         else:
-            return self.GET( url, args )
-    
+            # print("GET")
+            return self.GET(url, args)
+
+
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
+    # print(sys.argv)
     if (len(sys.argv) <= 1):
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print(client.command( sys.argv[2], sys.argv[1] ))
+        print(client.command(sys.argv[2], sys.argv[1]))
     else:
-        print(client.command( sys.argv[1] ))
+        print(client.command(sys.argv[1]))
